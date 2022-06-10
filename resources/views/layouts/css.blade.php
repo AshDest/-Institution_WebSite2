c576d7fb81e8662d97ba30a552756d3fa1a41231
unknown <ashuzamaheshe4@gmail.com>
2022-06-10 14:49:07 +0200

    other commit

---
+++ resources/views/layouts/css.blade.php
@@ -0,0 +1,16 @@
+        <!-- Favicon -->
+        <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/animate.min.css">
+        <link rel="stylesheet" href="css/meanmenu.css">
+        <link rel="stylesheet" href="css/magnific-popup.css">
+        <link rel="stylesheet" href="css/owl.carousel.min.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/et-line-icon.css">
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/ionicons.min.css">
+        <link rel="stylesheet" href="css/material-design-iconic-font.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/responsive.css">
+        <script src="js/vendor/modernizr-3.11.2.min.js"></script>
---
+++ resources/views/layouts/scripts.blade.php
@@ -0,0 +1,11 @@
+<script src="js/vendor/jquery-3.6.0.min.js"></script>
+<script src="js/vendor/jquery-migrate-3.3.2.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/jquery.meanmenu.js"></script>
+<script src="js/jquery.magnific-popup.js"></script>
+<script src="js/ajax-mail.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.mb.YTPlayer.js"></script>
+<script src="js/jquery.nicescroll.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
