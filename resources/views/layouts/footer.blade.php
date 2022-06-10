61fd97983d8d4e20cca5235d14b26e9752ddbc5e
unknown <ashuzamaheshe4@gmail.com>
2022-06-10 15:17:42 +0200

    other commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,67 @@
+        <!-- Footer Start -->
+        <footer class="footer-area">
+            <div class="main-footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6">
+                            <div class="single-widget pr-60">
+                                <div class="footer-logo pb-25">
+                                    <a href="index.html"><img src="img/logo/footer-logo.png" alt="eduhome"></a>
+                                </div>
+                                <p>I must explain to you how all this mistaken idea of denoung pleure and praising pain was born and give you a coete account of the system. </p>
+                                <div class="footer-social">
+                                    <ul>
+                                        <li><a href="https://www.facebook.com/devitems/?ref=bookmarks"><i class="zmdi zmdi-facebook"></i></a></li>
+                                        <li><a href="https://www.pinterest.com/devitemsllc/"><i class="zmdi zmdi-pinterest"></i></a></li>
+                                        <li><a href="#"><i class="zmdi zmdi-vimeo"></i></a></li>
+                                        <li><a href="https://twitter.com/devitemsllc"><i class="zmdi zmdi-twitter"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <div class="single-widget">
+                                <h3>information</h3>
+                                <ul>
+                                    <li><a href="#">addmission</a></li>
+                                    <li><a href="#">Academic Calender</a></li>
+                                    <li><a href="event.html">Event List</a></li>
+                                    <li><a href="#">Hostel &amp; Dinning</a></li>
+                                    <li><a href="#">TimeTable</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-md-6 pt-4 pt-lg-0">
+                            <div class="single-widget">
+                                <h3>useful links</h3>
+                                <ul>
+                                    <li><a href="course.html">our courses</a></li>
+                                    <li><a href="about.html">about us</a></li>
+                                    <li><a href="teacher.html">teachers &amp; faculty</a></li>
+                                    <li><a href="#">teams &amp; conditions</a></li>
+                                    <li><a href="event.html">our events</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 pt-4 pt-lg-0">
+                            <div class="single-widget">
+                                <h3>get in touch</h3>
+                                <p>Your address goes here, Street<br>City, Roadno 785 New York</p>
+                                <p>+880  548  986  898  87<br>+880  659  785  658  98</p>
+                                <p>info@eduhome.com<br>www.eduhome.com</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom text-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <p>Copyright © <a href="index.html">Eduhome</a> 2021. All Right Reserved By <a href="https://themeforest.net/user/hastech/portfolio" target="_blank">Hastech.</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Footer End -->
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,67 +1,108 @@
-        <!-- Footer Start -->
-        <footer class="footer-area">
-            <div class="main-footer">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-4 col-md-6">
-                            <div class="single-widget pr-60">
-                                <div class="footer-logo pb-25">
-                                    <a href="index.html"><img src="img/logo/footer-logo.png" alt="eduhome"></a>
+        <!-- Header Area Start -->
+		<header class="top">
+		    <div class="header-top">
+		        <div class="container">
+		            <div class="row">
+		                <div class="col-md-8 col-sm-8">
+		                    <div class="header-top-left">
+		                        <p>HAVE ANY QUESTION ?  +880 5698  598  6587</p>
+		                    </div>
+		                </div>
+		                <div class="col-md-4 col-sm-4">
+		                    <div class="header-top-right text-md-end text-center">
+		                        <ul>
+		                            <li><a href="login.html">login</a></li>
+		                            <li><a href="signup.html">signup</a></li>
+		                        </ul>
+		                    </div>
+		                </div>
+		            </div>
+		        </div>
+		    </div>
+			<div class="header-area two header-sticky">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3 col-sm-5 col-6">
+							<div class="logo">
+								<a href="index.html"><img src="img/logo/logo2.png" alt="eduhome" /></a>
+							</div>
+						</div>
+						<div class="col-md-9 col-sm-7 col-6">
+                            <div class="content-wrapper text-end">
+                                <!-- Main Menu Start -->
+                                <div class="main-menu">
+                                    <nav>
+                                        <ul>
+                                            <li><a href="index.html">Home</a>
+                                                <ul>
+                                                    <li><a href="index.html">Home One</a></li>
+                                                    <li><a href="index-2.html">Home Two</a></li>
+                                                    <li><a href="index-3.html">Home Three</a></li>
+                                                    <li><a href="index-4.html">Home Four</a></li>
+                                                    <li><a href="index-5.html">Home Five</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="about.html">About</a></li>
+                                            <li><a href="course.html">courses</a>
+                                                <ul>
+                                                    <li><a href="course.html">courses</a></li>
+                                                    <li><a href="course-details.html">courses details</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="event.html">event</a>
+                                                <ul>
+                                                    <li><a href="event.html">event</a></li>
+                                                    <li><a href="event-left-side-bar.html">event left sidebar</a></li>
+                                                    <li><a href="event-right-side-bar.html">event right sidebar</a></li>
+                                                    <li><a href="event-details.html">event details</a></li>
+                                                </ul>
+                                            </li>
+                                            <li class="hidden-sm"><a href="teacher.html">teacher</a>
+                                                <ul>
+                                                    <li><a href="teacher.html">teacher</a></li>
+                                                    <li><a href="teacher-details.html">teacher details</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="blog.html">blog</a>
+                                                <ul>
+                                                    <li><a href="blog.html">blog</a></li>
+                                                    <li><a href="blog-left-side-bar.html">blog left sidebar</a></li>
+                                                    <li><a href="blog-right-side-bar.html">blog righ sidebar</a></li>
+                                                    <li><a href="blog-details.html">blog details</a></li>
+                                                </ul>
+                                            </li>
+                                            <li><a href="contact.html">Contact</a></li>
+                                            <li><a href="contact.html">Buy Now</a>
+                                        </ul>
+                                    </nav>
                                 </div>
-                                <p>I must explain to you how all this mistaken idea of denoung pleure and praising pain was born and give you a coete account of the system. </p>
-                                <div class="footer-social">
-                                    <ul>
-                                        <li><a href="https://www.facebook.com/devitems/?ref=bookmarks"><i class="zmdi zmdi-facebook"></i></a></li>
-                                        <li><a href="https://www.pinterest.com/devitemsllc/"><i class="zmdi zmdi-pinterest"></i></a></li>
-                                        <li><a href="#"><i class="zmdi zmdi-vimeo"></i></a></li>
-                                        <li><a href="https://twitter.com/devitemsllc"><i class="zmdi zmdi-twitter"></i></a></li>
+                                <!--Search Form Start-->
+                                <div class="search-btn">
+                                    <ul class="header-search search-toggle">
+                                        <li class="search-menu">
+                                            <i class="fa fa-search"></i>
+                                        </li>
                                     </ul>
+                                    <div class="search">
+                                        <div class="search-form">
+                                            <form id="search-form" action="#">
+                                                <input type="search" placeholder="Search here..." name="search" />
+                                                <button type="submit">
+                                                    <span><i class="fa fa-search"></i></span>
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </div>
                                 </div>
+                                <!--End of Search Form-->
+                                <!-- Main Menu End -->
                             </div>
-                        </div>
-                        <div class="col-lg-3 col-md-6">
-                            <div class="single-widget">
-                                <h3>information</h3>
-                                <ul>
-                                    <li><a href="#">addmission</a></li>
-                                    <li><a href="#">Academic Calender</a></li>
-                                    <li><a href="event.html">Event List</a></li>
-                                    <li><a href="#">Hostel &amp; Dinning</a></li>
-                                    <li><a href="#">TimeTable</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-lg-2 col-md-6 pt-4 pt-lg-0">
-                            <div class="single-widget">
-                                <h3>useful links</h3>
-                                <ul>
-                                    <li><a href="course.html">our courses</a></li>
-                                    <li><a href="about.html">about us</a></li>
-                                    <li><a href="teacher.html">teachers &amp; faculty</a></li>
-                                    <li><a href="#">teams &amp; conditions</a></li>
-                                    <li><a href="event.html">our events</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-6 pt-4 pt-lg-0">
-                            <div class="single-widget">
-                                <h3>get in touch</h3>
-                                <p>Your address goes here, Street<br>City, Roadno 785 New York</p>
-                                <p>+880  548  986  898  87<br>+880  659  785  658  98</p>
-                                <p>info@eduhome.com<br>www.eduhome.com</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="footer-bottom text-center">
-                <div class="container">
-                    <div class="row">
+						</div>
                         <div class="col-12">
-                            <p>Copyright © <a href="index.html">Eduhome</a> 2021. All Right Reserved By <a href="https://themeforest.net/user/hastech/portfolio" target="_blank">Hastech.</a></p>
+                            <div class="mobile-menu hidden-sm"></div>
                         </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
-        <!-- Footer End -->
+					</div>
+				</div>
+			</div>
+		</header>
+		<!-- Header Area End -->
