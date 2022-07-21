2330d81b2884fd7860be74da07b23273a2bfb4f9
unknown <ashuzamaheshe4@gmail.com>
2022-07-21 15:47:21 +0200

    ash commit

--- app/Http/Livewire/Admin/Posts.php
+++ app/Http/Livewire/Admin/Posts.php
@@ -4,9 +4,11 @@ namespace App\Http\Livewire\Admin;
 
 use App\Models\Post;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Posts extends Component
 {
+    use WithFileUploads;
 
     public $titre;
     public $content;
@@ -15,49 +17,74 @@ class Posts extends Component
     public $ids;
 
 
-    protected $rules = [
-        'titre' => 'required',
-        'content' => 'required',
-        'image' => 'image|max:1024',
-    ];
+    // protected $rules = [
+    //     'titre' => 'required',
+    //     'content' => 'required',
+    //     'image' => 'image|max:1024',
+    // ];
 
-    protected $messages = [
-        'titre.required' => 'Le titre est obligatoire',
-        'content.required' => 'Le detail est obligatoire',
-        'image.required' => 'Selectionner un fichier',
-    ];
+    // protected $message = [
+    //     'titre.required' => 'Le titre est obligatoire',
+    //     'content.required' => 'Le detail est obligatoire',
+    //     'image.required' => 'Selectionner un fichier',
+    // ];
 
-    public function save()
+    public function update()
     {
-        $this->validate();
-        // Validate Form Request
+        $validatedData = $this->validate([
+            'titre' => 'required',
+            'content' => 'required',
+            'image' => 'image|max:1024',
+        ]);
         try {
-            $this->image->storeAs('Posts', $this->titre);
-            Post::create([
-                'titre' => ucfirst(trans($this->titre)),
-                'content' => ucfirst(trans($this->content)),
-                'image' => ucfirst(trans($this->image)),
-
-            ])->save();
+            $this->image->storeAs('Post', $this->titre);
+            Post::create($validatedData);
             $this->dispatchBrowserEvent('alert', [
                 'type' => 'success',
                 'message' => "Classe enregistée avec succes!!"
             ]);
-            // Reset Form Fields After Creating departement
         } catch (\Exception $e) {
-
-            dd($e->getMessage());
             // Set Flash Message
             $this->dispatchBrowserEvent('alert', [
                 'type' => 'error',
                 'message' => "Quelque chose ne va pas lors de l'enregistrement de la classe'!! " . $e->getMessage()
             ]);
-            // Reset Form Fields After Creating departement
         }
     }
+
+
+    // public function save()
+    // {
+    //     // $this->image->store('Posts');
+    //     $this->validate();
+    //     // Validate Form Request
+    //     try {
+    //         $this->image->store('Posts');
+    //         Post::create([
+    //             'titre' => ucfirst(trans($this->titre)),
+    //             'content' => $this->content,
+    //             'image' => $this->image,
+
+    //         ])->save();
+    //         $this->dispatchBrowserEvent('alert', [
+    //             'type' => 'success',
+    //             'message' => "Classe enregistée avec succes!!"
+    //         ]);
+    //         // Reset Form Fields After Creating departement
+    //     } catch (\Exception $e) {
+
+    //         dd($e->getMessage());
+    //         // Set Flash Message
+    //         $this->dispatchBrowserEvent('alert', [
+    //             'type' => 'error',
+    //             'message' => "Quelque chose ne va pas lors de l'enregistrement de la classe'!! " . $e->getMessage()
+    //         ]);
+    //         // Reset Form Fields After Creating departement
+    //     }
+    // }
     public function render()
     {
         $posts = Post::all();
-        return view('livewire.admin.posts', ['posts'=>$posts]);
+        return view('livewire.admin.posts', ['posts' => $posts]);
     }
 }
--- resources/views/livewire/admin/add_post.blade.php
+++ resources/views/livewire/admin/add_post.blade.php
@@ -1,6 +1,6 @@
 <!-- Modal -->
-<div wire:ignore.self class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+<div wire:ignore.self class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
+    tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -8,7 +8,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form>
+                <form enctype="multipart/form-data">
                     <div class="mb-3">
                         <label for="exampleFormControlInput1" class="form-label">Titre</label>
                         <input type="text" class="form-control" id="exampleFormControlInput1"
@@ -16,17 +16,19 @@
                     </div>
                     <div class="mb-3">
                         <label for="exampleFormControlTextarea1" class="form-label">Detail du Post</label>
-                        <textarea class="form-control" wire:model='detail' id="exampleFormControlTextarea1" rows="3"></textarea>
+                        <textarea class="form-control" wire:model='content' id="exampleFormControlTextarea1"
+                            rows="3"></textarea>
                     </div>
                     <div class="mb-3">
                         <label for="formFile" class="form-label">Default file input example</label>
-                        <input class="form-control" wire='file' type="file" id="formFile">
+                        <input class="form-control" wire:model='image' type="file" id="formFile">
+                        @error('image') <span class="error">{{ $message }}</span> @enderror
                     </div>
                 </form>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary" wire:click.prevent="save()">Understood</button>
+                <button type="button" class="btn btn-primary" wire:click.prevent="update()">Save</button>
             </div>
         </div>
     </div>
