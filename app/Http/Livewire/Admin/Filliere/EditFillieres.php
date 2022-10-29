3591f8da3c630c0428bdee05f838521109a60d91
unknown <ashuzamaheshe4@gmail.com>
2022-10-29 18:40:15 +0200

    Filliere adding Files

---
+++ app/Http/Livewire/Admin/Filliere/EditFillieres.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Filliere;
+
+use Livewire\Component;
+
+class EditFillieres extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.filliere.edit-fillieres');
+    }
+}
--- database/migrations/2022_07_19_142019_create_fillieres_table.php
+++ database/migrations/2022_07_19_142019_create_fillieres_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('designation');
             $table->text('detail');
+            $table->string('photo');
             $table->timestamps();
         });
     }
