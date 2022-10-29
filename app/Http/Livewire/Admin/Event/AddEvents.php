297005dd20a04d8ad3e8296b2b3b4e27d0cf9653
unknown <ashuzamaheshe4@gmail.com>
2022-10-29 23:29:49 +0200

    Adding Events Files

---
+++ app/Http/Livewire/Admin/Event/AddEvents.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Event;
+
+use Livewire\Component;
+
+class AddEvents extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.event.add-events');
+    }
+}
---
+++ app/Http/Livewire/Admin/Event/Events.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Event;
+
+use Livewire\Component;
+
+class Events extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.event.events');
+    }
+}
